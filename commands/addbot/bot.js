724a77e0d5d73d7b4bd04ff4168389abbbc358f6
Your Name <akshayaloklol@gmail.com>
2022-02-03 17:21:10 +0530

    Addbot 2.0

--- commands/addbot/bot.js
+++ commands/addbot/bot.js
@@ -2,10 +2,10 @@ module.exports = {
 name: "bot",
 code: `
 $if[$message[1]==accept]
-<@$mentioned[2]>,Your Bot was Accepted by <@$authorid>
+<@$message[3]>,Your Bot was Accepted by <@$authorid>
 $setUserVar[bot;false]
 $elseIf[$message[1]==decline]
-<@$mentioned[2>,Your Bot was Declined by <@$authorid>!]
+<@$message[3]>,Your Bot was Declined by <@$authorid>!]
 $setUserVar[bot;false]
 $endelseif
 $endif
