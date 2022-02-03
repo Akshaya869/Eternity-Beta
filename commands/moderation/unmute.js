0d63792678dc404e3dcff7d9ea0e85586c1021e5
Your Name <akshayaloklol@gmail.com>
2022-02-03 16:57:21 +0530

    Update 1.0.3

--- commands/moderation/unmute.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({
-  name: "unmute",
-  code: `
-  $takeRole[$mentioned[1];Muted]
-  **Successfully Unmuted <@$mentioned[1]>**
-  $supressErrors[**$getVar[no]Error! Muted Role is not created**]
-  $onlyPerms[kick;ban;**$getVar[no]You Cannot Use This Command (Less Perms,Require- Ban & Kick)**]
-  `
-})
