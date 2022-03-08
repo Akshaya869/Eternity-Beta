9f48a6610465c8d8c81b0a24ed3468e1c7399620
Your Name <akshayaloklol@gmail.com>
2022-03-08 21:00:25 +0530

    OK

--- commands/moderation/wlctoggle.js
+++ commands/moderation/wlctoggle.js
@@ -6,14 +6,14 @@ module.exports = {
     $onlyIf[$getServerVar[wlcToggle]==false;**$getVar[no]Welcome System is already enabled**]
     $endif
     $if[$message[1]==disable]
-    $description[**$getVar[yes] Successfully Disabled Welcome System]
+    **$getVar[yes] Successfully Disabled Welcome System
     $setServerVar[wlcToggle;false]
     $onlyIf[$getServerVar[wlcToggle]==true;**$getVar[no]Welcome System is already disabled**]
     $endif
     $if[$message[1]==]
-    $description[**Listen Mate you have 2 fields**
+   **Listen Mate you have 2 fields**
     \`e?toggle-welcome enable (to enable welcome)\`
                       or
-    \`e?toggle-welcome disable (to disable welcome)\`]
+    \`e?toggle-welcome disable (to disable welcome)\`
     `
 }
